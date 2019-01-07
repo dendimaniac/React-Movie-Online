f49de527a64c14c86528497d668ce2e81fdbf70c
Quan Dao <daq30899@gmail.com>
2019-01-07 22:12:04 +0200

    Basic structure + little styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import movies from "./static/movies";
 
-class App extends Component {
+class movieOnline extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="movie-header">
+          <img className="movie-image" alt="movie" src={movies.image} />
+          <p className="movie-name">{movies.name}</p>
+        </div>
+
+        <div className="buy-container">
+          <button className="buy-button">Buy for {movies.price}</button>
+        </div>
+
+        <div className="release-info-container">
+          <div className="release-info">{movies.release}</div>
+          <div className="release-info">{movies.genre}</div>
+        </div>
+
+        <div className="restriction-container">
+          <div className="restriction">FullHD</div>
+          <div className="restriction">18+</div>
+        </div>
+
+        <div className="basic-info-container">
+          <div className="basic-info">
+            <div>Length</div>
+            <div>Genre</div>
+            <div>Director</div>
+          </div>
+          <div className="basic-info">
+            <div>{movies.length.hour} hour {movies.length.minute} minutes</div>
+            <div>{movies.genre}</div>
+            <div>{movies.director}</div>
+          </div>
+        </div>
+
+        <div className="rating-container">
+          <div className="rating">{movies.rating}</div>
+          <div className="rating-name">IMDb</div>
+        </div>
+
+        <div className="description-container">
+          <div className="description-header">Description</div>
+          <div className="description-text">{movies.description}</div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default movieOnline;
---
+++ src/static/movies.js
@@ -0,0 +1,106 @@
+import transformers from './images/transformers.jpg'
+import theAvengers from './images/the-avengers.jpg'
+import needForSpeed from './images/need-for-speed.jpg'
+import thorDarkWorld from './images/thor-the-dark-world.jpg'
+import ralph from './images/ralph-breaks-the-internet.jpg'
+
+export default {
+    image: transformers,
+    name: "Transformers",
+    price: "60e",
+    release: "2007, USA",
+    genre: "Actions, Adventures, Sci-Fi, Fantasy",
+    length: {
+      hour: 2,
+      minute: 23
+    },
+    rating: "7.1/10",
+    director: "Michael Bay",
+    language: "English, Russian, German, Japanese",
+    description:
+      "An ancient struggle between two Cybertronian races, the heroic Autobots and the evil Decepticons, comes to Earth, with a clue to the ultimate power held by a teenager."
+}
+
+/* export default [
+  {
+    image: transformers,
+    name: "Transformers",
+    price: "60e",
+    release: "2007, USA",
+    genre: "Actions, Adventures, Sci-Fi, Fantasy",
+    length: {
+      hour: 2,
+      minute: 23
+    },
+    rating: "7.1/10",
+    director: "Michael Bay",
+    language: "English, Russian, German, Japanese",
+    description:
+      "An ancient struggle between two Cybertronian races, the heroic Autobots and the evil Decepticons, comes to Earth, with a clue to the ultimate power held by a teenager."
+  },
+  {
+    image: theAvengers,
+    name: "The Avengers",
+    price: "60e",
+    release: "2012, USA",
+    genre: "Actions, Adventures, Sci-Fi, Fantasy",
+    length: {
+      hour: 2,
+      minute: 24
+    },
+    rating: "8.1/10",
+    director: "Joss Whedon",
+    language: "English, Russian, German, Japanese",
+    description:
+      "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity."
+  },
+  {
+    image: needForSpeed,
+    name: "Need for Speed",
+    price: "60e",
+    release: "2014, USA",
+    genre: "Actions, Adventures, Thriller",
+    length: {
+      hour: 2,
+      minute: 5
+    },
+    rating: "6.5/10",
+    director: "Scott Waugh",
+    language: "English, Russian, German, Japanese",
+    description:
+      "Fresh from prison, a street racer who was framed by a wealthy business associate joins a cross country race with revenge in mind. His ex-partner, learning of the plan, places a massive bounty on his head as the race begins."
+  },
+  {
+    image: thorDarkWorld,
+    name: "Thor: The Dark World",
+    price: "60e",
+    release: "2013, USA",
+    genre: "Actions, Adventures, Superhero",
+    length: {
+      hour: 1,
+      minute: 53
+    },
+    rating: "7.0/10",
+    director: "Alan Taylor",
+    language: "English, Russian, German, Japanese",
+    description:
+      "When Dr. Jane Foster gets cursed with a powerful entity known as the Aether, Thor is heralded of the cosmic event known as the Convergence and the genocidal Dark Elves."
+  },
+  {
+    image: ralph,
+    name: "Ralph Breaks the Internet",
+    price: "60e",
+    release: "2018, USA",
+    genre: "Animation, Adventures, Comedy, Fantasy",
+    length: {
+      hour: 1,
+      minute: 52
+    },
+    rating: "7.4/10",
+    director: "Phil Johnston, Rich Moore",
+    language: "English, Russian, German, Japanese",
+    description:
+      "Six years after the events of 'Wreck-It Ralph,' Ralph and Vanellope, now friends, discover a wi-fi router in their arcade, leading them into a new adventure."
+  }
+];
+ */
